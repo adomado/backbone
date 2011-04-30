0988a1e83c78119e0475c67beab039181e1ecf10
makuchaku <makuchaku@adomado.com>
2011-04-30 18:15:12 +0530

    Added backend functionality to post comments

--- app.rb
+++ app.rb
@@ -20,6 +20,8 @@ end
 # Should return JSON - as response can be consumed by an ajax call
 # Required params are post_url (encoded), post_data (encoded)
 get '/post' do
-  post_command = "curl -F '#{CGI.unescape(params[:post_data])}' '#{CGI.unescape(params[:post_url])}'"
+  post_command = "curl -F '#{CGI.unescape(params["access_token"])}'"
+  post_command += " -F '#{CGI.unescape(params["query"])}'" if params["query"] # not passed in like, but is passed in comment...
+  post_command += " '#{CGI.unescape(params["post_url"])}'"
   return IO.popen(post_command).read
 end
--- fb/lib/fb_action.js
+++ fb/lib/fb_action.js
@@ -13,17 +13,30 @@ var FBAction = new JS.Class({
   like : function(fbItemId, postCallback) {
     this.fbItemId = fbItemId;
     var postUrl = encodeURIComponent(this.graphUrl + fbItemId + "/likes");
-    var postData = encodeURIComponent("access_token=" + this.fbAccessToken);
+    var accessToken = encodeURIComponent("access_token=" + this.fbAccessToken);
+    var postData = {accessToken : accessToken};
     
     this.get(postUrl, postData, function(){
       postCallback.call();
     });
   },
+
+
+  comment : function(fbItemId, commentText, postCallback) {
+    var postUrl = encodeURIComponent(this.graphUrl + fbItemId + "/comments");
+    var accessToken = encodeURIComponent("access_token=" + this.fbAccessToken);
+    var comment = encodeURIComponent("message=" + commentText);
+    var postData = {accessToken : accessToken, data : comment};
+    
+    this.get(postUrl, postData, function(){
+      postCallback.call();
+    });    
+  },
   
   
   get : function(postUrl, postData, successCallback) {
     successCallback = successCallback || function(data) {};
-    $.getJSON(this.postProxy, {post_data : postData, post_url : postUrl}, successCallback);
+    $.getJSON(this.postProxy, {access_token : postData.accessToken, query : postData.data, post_url : postUrl}, successCallback);
   }
 
 });
