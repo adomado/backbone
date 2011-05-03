8e884ef299b4ed563cdf17832bd2215e367243b8
makuchaku <makuchaku@adomado.com>
2011-05-03 18:11:10 +0530

    Foo commit

--- fb/lib/fb_auth.js
+++ fb/lib/fb_auth.js
@@ -9,10 +9,10 @@ var FBAuth = new JS.Class({
       this.authUrl = "https://graph.facebook.com/oauth/authorize?client_id=" + appId + "&redirect_uri=" + returnUrl +
                       "&scope=publish_stream,offline_access,email,read_stream,user_about_me,user_birthday,user_education_history," +
                       "user_hometown,user_online_presence,user_relationships,user_status&response_type=token";
-                      
+      
       if(this.needAuth() == true)
         document.location.href = this.getAuthUrl();                      
-                      
+
       this.getAccessTokenFromUrl();                      
     },
     
