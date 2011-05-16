164ab5362885c29cc93a360fc5d969b2cb4ce7c8
makuchaku <makuchaku@adomado.com>
2011-05-16 14:05:22 +0530

    Prepared the FB connect page

--- fb/lib/fb_auth.js
+++ fb/lib/fb_auth.js
@@ -9,33 +9,34 @@ var FBAuth = new JS.Class({
       this.authUrl = "https://graph.facebook.com/oauth/authorize?client_id=" + appId + "&redirect_uri=" + returnUrl +
                       "&scope=publish_stream,offline_access,email,read_stream,user_about_me,user_birthday,user_education_history," +
                       "user_hometown,user_online_presence,user_relationships,user_status&response_type=token";
-      
-      if(this.needAuth() == true)
-        document.location.href = this.getAuthUrl();                      
 
-      this.getAccessTokenFromUrl();                      
+      if(this.needAuth() == true)
+        $.mobile.changePage($("#fb-auth-page"));
+
+      this.getAccessTokenFromUrl();
     },
-    
-    
+
+
     getAuthUrl : function() {
       return this.authUrl;
     },
-    
-    
+
+
     needAuth : function() {
-      if(this.getItem("fbAccessToken") == null)
+      if((this.getItem("fbAccessToken") == null) || (this.getItem("fbAccessToken") == undefined))
         return true;
       else
         return false;
     },
-    
-    
+
+
+    // TODO - seems not to be used anymore...?
     reinforceAuth : function() {
       this.removeItem("fbAccessToken");
       this.reinforceAuthCallback.call();
     },
-    
-    
+
+
     getAccessTokenFromUrl : function() {
       try {
         if(document.location.hash != "")
